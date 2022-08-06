967418c648bd28f4b9db0979ea90b7f01ce37982
porrasmdz <aporrasmdz@gmail.com>
2022-08-05 20:56:54 -0500

    pageview upgrd

---
+++ src/components/GenericPageHeader.vue
@@ -0,0 +1,34 @@
+<script setup>
+import FullScreenDiv from "@/components/FullScreenDiv.vue";
+</script>
+<template>
+  <FullScreenDiv class="h-[50vh] flex">
+    <div class="w-1/3 flex flex-col gap-12 p-8 text-left">
+      <div>
+        <h1 class="text-blue-500 text-4xl mb-2">
+          Marco Legal Teorico (Titulo)
+        </h1>
+        <h2 class="text-gray-500 text-xl mb-2">
+          ¡Actualizado a diario! (Subtitulo)
+        </h2>
+      </div>
+
+      <div class="justify-self-end">
+        <p>
+        <span class="font-bold">Fecha de creación:</span>
+        dd/mm/aa</p>
+        <p>
+        <span class="font-bold">Fecha de última actualización:</span>
+        dd/mm/aa</p>
+      </div>
+      
+    </div>
+    <div class="w-2/3 overflow-hidden">
+      <img
+        src="https://source.unsplash.com/1600x900/?business"
+        class="object-cover w-full h-[50vh]"
+        alt=""
+      />
+    </div>
+  </FullScreenDiv>
+</template>
--- src/views/PageView.vue
+++ src/views/PageView.vue
@@ -1,34 +1,46 @@
 <script setup>
 import FullScreenDiv from "@/components/FullScreenDiv.vue";
+import GenericPageHeader from "@/components/GenericPageHeader.vue";
+import CardList from "@/components/CardList.vue";
 </script>
 <template>
-  <FullScreenDiv class="h-[50vh] flex">
-    <div class="w-1/3 flex flex-col gap-12 p-8 text-left">
-      <div>
-        <h1 class="text-blue-500 text-4xl mb-2">
-          Marco Legal Teorico (Titulo)
-        </h1>
-        <h2 class="text-gray-500 text-xl mb-2">
-          ¡Actualizado a diario! (Subtitulo)
-        </h2>
-      </div>
-
-      <div class="justify-self-end">
-        <p>
-        <span class="font-bold">Fecha de creación:</span>
-        dd/mm/aa</p>
-        <p>
-        <span class="font-bold">Fecha de última actualización:</span>
-        dd/mm/aa</p>
-      </div>
-      
-    </div>
-    <div class="w-2/3 overflow-hidden">
-      <img
-        src="https://source.unsplash.com/1600x900/?business"
-        class="object-cover w-full h-[50vh]"
-        alt=""
-      />
-    </div>
-  </FullScreenDiv>
+  <div class="h-auto">
+    <GenericPageHeader class="shadow-2xl"></GenericPageHeader>
+    <FullScreenDiv
+      class="h-[50vh] mx-auto bg-gray-100 flex justify-center pb-4"
+    >
+      <CardList class="-mt-20">
+        <template #list-title>Las últimas publicaciones de la vista</template>
+      </CardList>
+    </FullScreenDiv>
+    <FullScreenDiv class="min-h-[50vh] h-auto py-24">
+      <ul class="grid grid-cols-3">
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+        <li>
+          <a href="" class="text-blue-600 hover:underline">Categoria N</a>
+        </li>
+      </ul>
+    </FullScreenDiv>
+  </div>
 </template>
