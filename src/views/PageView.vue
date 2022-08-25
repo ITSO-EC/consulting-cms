7ea596257dee20a2383ee6b5660163ff7f8e9e75
porrasmdz <aporrasmdz@gmail.com>
2022-08-24 22:37:29 -0500

    responsive fix

--- src/views/PageView.vue
+++ src/views/PageView.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="flex pt-12 justify-center items-center inset-0">
+  <div class="flex py-12 justify-center items-center inset-0">
       <div class="w-52 gap-4 flex flex-col">
         <img class="w-44" src="../assets/images/empty.png" alt="Avion de papel">
         <span class="text-xl font-bold">Haga click sobre una categoría para empezar a navegar</span>
