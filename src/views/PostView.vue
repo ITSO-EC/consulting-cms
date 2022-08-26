11d9b5f779879069c1db7b1bd7d622ff3b5531b1
porrasmdz <aporrasmdz@gmail.com>
2022-08-26 13:32:26 -0500

    mobile pdf view

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,6 +1,6 @@
 <script setup>
   import PostCard from "@/components/PostCard.vue";
-  import VuePdfEmbed from 'vue-pdf-embed'
+  import MobilePdfViewer from "@/components/MobilePdfViewer.vue"
 </script>
 <template>
   <div class="h-auto pb-16">
@@ -9,11 +9,10 @@
 
     <h1>File:</h1>
     <iframe 
-    class="hidden sm:block border border-gray-200 shadow-xl max-w-screen w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
+    class="hidden md:block border border-gray-200 shadow-xl max-w-screen w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
     :src="'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'" />
-    <VuePdfEmbed 
-    class="sm:hidden border my-8 border-gray-200 shadow-xl w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto" 
-    :page="1" 
-    :source="'https://cryptic-dawn-02543.herokuapp.com/https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'" />
+    
+    <!-- Requiere un proxy, estoy usando uno clonado y hosteado en heroku -->
+    <MobilePdfViewer class="md:hidden"/>
   </div>
 </template>
