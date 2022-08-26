a96d66fc5ae2ba8261efc1a13af75a73f628933b
porrasmdz <aporrasmdz@gmail.com>
2022-08-26 11:58:36 -0500

    preview on mobile

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,6 +1,6 @@
 <script setup>
   import PostCard from "@/components/PostCard.vue";
-  //import VuePdfEmbed from 'vue-pdf-embed'
+  import VuePdfEmbed from 'vue-pdf-embed'
 </script>
 <template>
   <div class="h-auto pb-16">
@@ -9,11 +9,11 @@
 
     <h1>File:</h1>
     <iframe 
-    class="border border-gray-200 shadow-xl max-w-screen w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
+    class="hidden sm:block border border-gray-200 shadow-xl max-w-screen w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
     :src="'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'" />
-    <!-- <VuePdfEmbed 
-    class="border border-gray-200 shadow-xl max-w-screen sm:w-5/6 mx-2 sm:mx-auto" 
+    <VuePdfEmbed 
+    class="sm:hidden border my-8 border-gray-200 shadow-xl w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto" 
     :page="1" 
-    :source="'../../Proyecto.pdf'" /> -->
+    :source="'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'" />
   </div>
 </template>
