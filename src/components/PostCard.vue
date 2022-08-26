54a8f178e58dc0c0ed3154de8505e0a4ceb5227e
porrasmdz <aporrasmdz@gmail.com>
2022-08-26 11:07:53 -0500

    pdf viewer

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -2,15 +2,15 @@
 <template>
   <!-- Mover esta tabla a un componente -->
   <div class="w-auto sm:w-5/6 mx-2 sm:px-0 sm:mx-auto shadow-xl rounded-sm border border-gray-200">
-    <div class="grid grid-cols-6 w-full">
-      <div class="col-span-1 flex flex-col items-center justify-center gap-2 m-4">
+    <div class="grid grid-cols-12 w-full">
+      <div class="col-span-12 sm:col-span-3 lg:col-span-2 flex flex-col items-center justify-center gap-2 m-4">
         <span class="font-bold h-1/3">Órgano Emisor</span>
         <img src="../assets/logos/sriLogo.png" class="h-24 w-24" alt="SRILogo" />
   
       </div>  
-      <div class="col-span-4">
-        <div class="grid grid-cols-4 p-4">
-          <span :class="`text-[${primaryColor}] col-span-4 font-bold text-left`">Reforma en la declaracion de IVA</span>
+      <div class="col-span-12 sm:col-span-6 lg:col-span-8">
+        <div class="grid grid-cols-2 lg:grid-cols-4 p-4">
+          <span :class="`text-[${primaryColor}] col-span-2 lg:col-span-4 font-bold text-left`">Reforma en la declaracion de IVA</span>
           <span :class="`text-[${primaryColor}] font-bold text-left`">R.O.:</span>
           <span class="text-justify">9123</span>
           <span :class="`text-[${primaryColor}] font-bold text-left`">Fecha:</span>
@@ -23,20 +23,20 @@
           <span class="text-justify">543</span>
         </div>
       </div>
-      <div class="col-span-1  flex flex-col items-center justify-center gap-2 m-4">
+      <div class="col-span-12 sm:col-span-3 lg:col-span-2  flex flex-col items-center justify-center gap-2 m-4">
         <span class="font-bold h-1/3">Descargar PDF</span>
         <BaseIcon :size="'h-24'" class="cursor-pointer" :name="'file'" />
       </div>
     </div>
-    <div class="col-span-6 h-40 text-left mx-4 py-4 border-t border-gray-200">
+    <div class="col-span-12 min-h-40 h-auto px-2 text-left mx-4 py-4 border-t border-gray-200">
       <span :class="`text-[${primaryColor}] font-bold text-left`">Descripción:</span>
-      Esta es mi descripcion
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque animi eos, provident autem doloribus magni ut voluptate facere quibusdam dolorum totam vitae. Iste quam temporibus ad ea quod aliquid itaque.
     </div>
-    <div class="col-span-6 h-10 p-2 text-left mx-4 border-t border-gray-200">
-      <span :class="`text-[${primaryColor}] font-bold text-left`">Referencia:</span>
-      Esta es mi descripcion
+    <div class="col-span-12 h-10 p-2 text-left mx-4 border-t text-xs sm:text-sm border-gray-200">
+      <span :class="`text-[${primaryColor}] font-bold text-left text-md`">Referencia:</span>
+      www.sri.gob.ec
     </div>
-      
+    
       
   </div>
 </template>
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,9 +1,19 @@
 <script setup>
   import PostCard from "@/components/PostCard.vue";
+  //import VuePdfEmbed from 'vue-pdf-embed'
 </script>
 <template>
   <div class="h-auto pb-16">
     
     <PostCard/>
+
+    <h1>File:</h1>
+    <iframe 
+    class="border border-gray-200 shadow-xl max-w-screen w-[97vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
+    :src="'../../Proyecto.pdf'" />
+    <!-- <VuePdfEmbed 
+    class="border border-gray-200 shadow-xl max-w-screen sm:w-5/6 mx-2 sm:mx-auto" 
+    :page="1" 
+    :source="'../../Proyecto.pdf'" /> -->
   </div>
 </template>
