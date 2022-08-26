45e208d3b1e94446ca24f6f517c1a880de140470
porrasmdz <aporrasmdz@gmail.com>
2022-08-26 11:42:09 -0500

    https pdf

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -9,8 +9,8 @@
 
     <h1>File:</h1>
     <iframe 
-    class="border border-gray-200 shadow-xl max-w-screen w-[96.5vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
-    :src="'http://www.africau.edu/images/default/sample.pdf'" />
+    class="border border-gray-200 shadow-xl max-w-screen w-[95vw] sm:w-5/6 min-h-[100vh] h-fit mx-2 sm:mx-auto"  
+    :src="'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'" />
     <!-- <VuePdfEmbed 
     class="border border-gray-200 shadow-xl max-w-screen sm:w-5/6 mx-2 sm:mx-auto" 
     :page="1" 
