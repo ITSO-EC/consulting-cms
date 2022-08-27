1fe1eb6fc0d650a5abe10757bb4ef3f5a0aab61e
porrasmdz <aporrasmdz@gmail.com>
2022-08-27 02:42:43 -0500

    rescaled logo

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/" class="flex items-center justify-self-start">
-    <img src="https://acorpconsulting.com/images/headers/logo_consulting.png" class="mr-3 h-6 sm:h-9 grayscale brightness-200" alt="" />
+    <img src="https://acorpconsulting.com/images/headers/logo_consulting.png" class="mr-3 h-10 sm:h-12 -my-2 grayscale brightness-200" alt="" />
     <!-- <span
       :class="`text-[${blankColor}]`"
       class="self-center text-xl font-semibold whitespace-nowrap"
