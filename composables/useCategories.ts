fcce2698c10bea3d0ae5a623de8db850aeff373e
porrasmdz <aporrasmdz@gmail.com>
2022-09-20 03:39:00 -0500

    api fix

--- composables/useCategories.ts
+++ composables/useCategories.ts
@@ -1,13 +1,14 @@
 import { storeToRefs } from 'pinia';
 import { useCategoriesStore } from '~~/stores/categoriesStore'
 
+const BASE_API='https://itso.ga/v1/'
 const useCategories = () => {
     const categoriesStore = useCategoriesStore();
     
     
     const {categories, selectedCategory,error, loading, results, page} = storeToRefs(categoriesStore);
 
-    const initializeCategories = async (id: string) => categoriesStore.loadCategories(await $fetch('/api/categories?byPage='+id));
+    const initializeCategories = async (id: string) => categoriesStore.loadCategories(await $fetch(BASE_API+'categories?byPage='+id));
     const getCategoryById = (id: string) => categoriesStore.getCategoryById(id);
     return {
         // Properties
--- composables/useQueryPosts.ts
+++ composables/useQueryPosts.ts
@@ -1,16 +1,18 @@
 import { storeToRefs } from 'pinia';
 import { useQueryPostsStore } from '~~/stores/queryPostsStore'
 
+const BASE_API='https://itso.ga/v1/'
+
 const useQueryPosts = () => {
     const queryPostsStore = useQueryPostsStore();
     
     const {posts, selectedPost, error, loading, results, page} = storeToRefs(queryPostsStore);
 
     const initializeAllPosts = async()=>{
-        queryPostsStore.loadPosts(await $fetch('/api/posts'))
+        queryPostsStore.loadPosts(await $fetch(BASE_API+'posts'))
     };
     const initializeQueriedPosts = async(id: string)=>{
-        queryPostsStore.loadPosts(await $fetch('/api/posts?byCategory='+id))
+        queryPostsStore.loadPosts(await $fetch(BASE_API+'posts?byCategory='+id))
     };
     const selectPostById = (id: string) => queryPostsStore.getPostById(id);
 
